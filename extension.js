745d41a0dc07663044a2ae4eb3a965277f35d10b
Artyom Babiy <arthem.babij@gmail.com>
2023-09-03 09:02:07 +0300

    style: a few small tweaks

--- extension.js
+++ extension.js
@@ -17,26 +17,26 @@ export default class TrimmerExtension extends Extension {
             return;
         }
 
-        let trimMode = this._settings.get_int("trim-mode");
-        let trimString = this._settings.get_string("trim-string");
+        const trimMode = this._settings.get_int("trim-mode");
+        const trimString = this._settings.get_string("trim-string");
         let trimText;
 
         if (trimMode === 0) {
             trimText = text.trim();
             if (trimString) {
-                let regex = new RegExp(`^${trimString}|${trimString}$`);
+                const regex = new RegExp(`^${trimString}|${trimString}$`);
                 trimText = trimText.replace(regex, "");
             }
         } else if (trimMode === 1) {
             trimText = text.trimLeft();
             if (trimString) {
-                let regex = new RegExp(`^${trimString}`);
+                const regex = new RegExp(`^${trimString}`);
                 trimText = trimText.replace(regex, "");
             }
         } else if (trimMode === 2) {
             trimText = text.trimRight();
             if (trimString) {
-                let regex = new RegExp(`${trimString}$`);
+                const regex = new RegExp(`${trimString}$`);
                 trimText = trimText.replace(regex, "");
             }
         }
@@ -48,6 +48,7 @@ export default class TrimmerExtension extends Extension {
         if (trimText === text) {
             return;
         }
+
         this._clipboard.set_text(St.ClipboardType.CLIPBOARD, trimText);
     }
 
@@ -59,10 +60,8 @@ export default class TrimmerExtension extends Extension {
 
     enable() {
         this._settings = this.getSettings();
-
         this._clipboard = St.Clipboard.get_default();
         this._selection = Shell.Global.get().get_display().get_selection();
-
         this._trimId = this._selection.connect("owner-changed", (_, selectionType) => {
             if (selectionType === Meta.SelectionType.SELECTION_CLIPBOARD) {
                 this._getClipboardContent();
